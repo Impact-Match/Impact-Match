3e438bc7ef94e5146fef3b5bf359f085349d75d0
zhunan123 <zhunan@bu.edu>
2024-09-22 18:59:29 -0700

    initial commit for signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Signup from './Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // only for demo signup page, we can change it later
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/signup">
+            <button className="py-2 px-4 bg-blue-500 text-white rounded-lg">
+              Sign Up
+            </button>
+          </Link>
+        </header>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
