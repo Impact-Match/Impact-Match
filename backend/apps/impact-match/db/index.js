05f1180670db6ff8650625c2963a14894f213f7e
Junjia Wang <jwang268@dons.usfca.edu>
2024-09-24 10:30:38 -0700

    update env and correct README

--- backend/apps/impact-match/db/index.js
+++ backend/apps/impact-match/db/index.js
@@ -1,7 +1,9 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+
+require("dotenv").config(); // Load environment variables from a .env file
 
 const pool = new Pool({
-  connectionString: "postgres://u563hvr13bgmbh:pdffe5ffc8502bd05fdbb50d3782e269f72eda6dd6c0b8c7e25c7086ec32110ce@cd5gks8n4kb20g.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d8m17f0ud96jts",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // Use this only for development
   },
