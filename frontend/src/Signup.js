3b7ec4feb1d4c097077c0dd39504943f6336ab15
zhunan123 <zhunan@bu.edu>
2024-09-30 14:11:20 -0700

    resolve signup style change

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import icon from './assets/ImpactMatchLogo_Transparent.png';
 import googleIcon from './assets/google.png'; 
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     const [userType, setUserType] = useState('student');
@@ -34,7 +35,7 @@ const Signup = () => {
                     {/* Right Section */}
                     <div className="w-full md:w-2/3 p-10 flex flex-col items-center justify-center">
                     <h2 className="text-2xl mb-6 text-gray-600 text-center font-inter">Create New Account</h2>
-                    <p className="text-sm mb-28 text-gray-400 text-center">Already Registered? <button className="text-lightBlue">Login</button></p>
+                    <p className="text-sm mb-28 text-gray-400 text-center">Already Registered? <Link to="/login" className="text-lightBlue">Login</Link></p>
                         {userType === 'student' ? (
                             <form className="w-full space-y-4">
                                 <div className="flex flex-col items-center">
@@ -71,12 +72,6 @@ const Signup = () => {
                                         className="w-[17%] py-2 bg-lightBlue text-black rounded-2xl text-sm mt-6"
                                     >
                                         Sign Up
-                                    </button>
-                                </div>
-                                <div className="flex justify-center mt-4">
-                                    <button className="min-w-[180px] py-2 flex items-center justify-center bg-white text-black border border-gray-300 rounded-2xl text-sm">
-                                        <img src={googleIcon} alt="Google Icon" className="w-5 h-5 mr-2" />
-                                        Sign in with Google
                                     </button>
                                 </div>
                             </form>
@@ -116,12 +111,6 @@ const Signup = () => {
                                         className="w-[17%] py-2 bg-lightBlue text-black rounded-2xl text-sm mt-6"
                                     >
                                         Sign Up
-                                    </button>
-                                </div>
-                                <div className="flex justify-center mt-4">
-                                    <button className="min-w-[180px] py-2 flex items-center justify-center bg-white text-black border border-gray-300 rounded-2xl text-sm">
-                                        <img src={googleIcon} alt="Google Icon" className="w-5 h-5 mr-2" />
-                                        Sign in with Google
                                     </button>
                                 </div>
                             </form>
