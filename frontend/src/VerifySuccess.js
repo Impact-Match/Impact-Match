4bd69e98e747511787746f9ef4e0103d7f7d0f2c
Junjia Wang <jwang268@dons.usfca.edu>
2024-10-08 22:55:29 -0700

    add all

--- frontend/src/VerifySuccess.js
+++ frontend/src/VerifySuccess.js
@@ -1,43 +1,51 @@
-import React from 'react';
-import icon from './assets/ImpactMatchLogo_Transparent.png';
-import SuccessSendIcon from './assets/SuccessSend.png';
+import React from "react";
+import icon from "./assets/ImpactMatchLogo_Transparent.png";
+import SuccessSendIcon from "./assets/SuccessSend.png";
 
 const VerifySuccess = ({ isNgoVerified, isAdmin }) => {
-    return (
-        <div className="flex justify-center items-center h-screen w-full">
-            <div className="flex flex-col items-center w-full h-full bg-darkBlue p-8">
-                <img src={icon} alt="Impact Match Logo" className="w-48 h-48" />
-                <h1 className="text-4xl font-inter text-white mb-4">Impact Match</h1>
-                
-                {/* Email Icon */}
-                <div className="w-full flex justify-center mb-2">
-                    <img src={SuccessSendIcon} alt="Email Success" className="w-32 h-32 object-contain" />
-                </div>
-                
-                <p className="text-sm mb-12 text-white text-center">Your email has been successfully verified</p>
-                {!isNgoVerified && (
-                    <p className="text-sm mb-12 text-yellow-300 text-center">
-                        Email verification successful, but your account still needs NGO admin approval. Please wait for further notification.
-                    </p>
-                )}
-                {isAdmin && (
-                    <p className="text-sm mb-12 text-yellow-300 text-center">
-                        NGO verification successful. You can now log in to your account.
-                    </p>
-                )}
-                
-                {/* Log In Button */}
-                <div className="flex justify-center">
-                    <button
-                        type="submit"
-                        className="w-48 h-9 py-1.5 px-2 bg-lightBlue text-darkblue rounded-3xl text-sm transition-shadow duration-150 hover:bg-blue-800 hover:text-white active:bg-blue-900 active:shadow-inner"
-                    >
-                        Finish Account Set Up
-                    </button>
-                </div>
-            </div>
+  console.log(isNgoVerified, isAdmin);
+  return (
+    <div className="flex justify-center items-center h-screen w-full">
+      <div className="flex flex-col items-center w-full h-full bg-darkBlue p-8">
+        <img src={icon} alt="Impact Match Logo" className="w-48 h-48" />
+        <h1 className="text-4xl font-inter text-white mb-4">Impact Match</h1>
+
+        {/* Email Icon */}
+        <div className="w-full flex justify-center mb-2">
+          <img
+            src={SuccessSendIcon}
+            alt="Email Success"
+            className="w-32 h-32 object-contain"
+          />
         </div>
-    );
-}
+
+        <p className="text-sm mb-12 text-white text-center">
+          Your email has been successfully verified
+        </p>
+        {!isNgoVerified && (
+          <p className="text-sm mb-12 text-yellow-300 text-center">
+            Email verification successful, but your account still needs NGO
+            admin approval. Please wait for further notification.
+          </p>
+        )}
+        {isAdmin && (
+          <p className="text-sm mb-12 text-yellow-300 text-center">
+            NGO verification successful. You can now log in to your account.
+          </p>
+        )}
+
+        {/* Log In Button */}
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="w-48 h-9 py-1.5 px-2 bg-lightBlue text-darkblue rounded-3xl text-sm transition-shadow duration-150 hover:bg-blue-800 hover:text-white active:bg-blue-900 active:shadow-inner"
+          >
+            Finish Account Set Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default VerifySuccess;
